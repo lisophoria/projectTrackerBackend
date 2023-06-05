43c6da38fb28768d5b3b651816b84676c7b7604b
Victor Astakhov <work.lisophoria@gmail.com>
2023-06-05 03:55:17 +0300

    migrate database & fix cors

--- src/main/java/com/lisophoria/projectTracker/config/JwtAuthenticationFilter.java
+++ src/main/java/com/lisophoria/projectTracker/config/JwtAuthenticationFilter.java
@@ -32,6 +32,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       @NonNull HttpServletResponse response,
       @NonNull FilterChain filterChain
   ) throws ServletException, IOException {
+
+    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
     if (request.getServletPath().contains("/api/v1/auth")) {
       filterChain.doFilter(request, response);
       return;
